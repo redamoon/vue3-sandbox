5ef35401f52124da7f3c2bee524a47dd088def03
redamoon <yuuki60824@gmail.com>
2023-05-02 08:38:50 +0900

    add storybook

--- src/components/MyButton/MyButton.stories.ts
+++ src/components/MyButton/MyButton.stories.ts
@@ -3,16 +3,41 @@ import type { Meta, StoryObj } from '@storybook/vue3'
 
 type Story = StoryObj<typeof MyButton>
 
+type Args = {
+  label: string
+  variant: string
+}
+
 const meta: Meta<typeof MyButton> = {
   title: 'MyButton',
   component: MyButton
 }
 
 export const Default: Story = {
-  render: () => ({
+  render: (args: Args) => ({
     components: { MyButton },
-    template: "<MyButton label='ボタン' />"
-  })
+    setup() {
+      return { args }
+    },
+    template: "<MyButton v-bind='args' />"
+  }),
+  args: {
+    label: 'ボタン'
+  }
+}
+
+export const Login: Story = {
+  render: (args: Args) => ({
+    components: { MyButton },
+    setup() {
+      return { args }
+    },
+    template: "<MyButton v-bind='args' />"
+  }),
+  args: {
+    label: 'ログイン',
+    variant: 'secondary'
+  }
 }
 
 export default meta
