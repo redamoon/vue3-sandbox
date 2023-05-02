0ab1b07c7f470f1f343a30fd777ee1c155b20dd8
redamoon <yuuki60824@gmail.com>
2023-05-02 10:17:45 +0900

    fix

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,8 @@ import type { StorybookConfig } from '@storybook/vue3-vite'
 
 const config: StorybookConfig = {
   framework: '@storybook/vue3-vite',
-  stories: ['../src/**/*.stories.@(js|ts)']
+  stories: ['../src/**/*.stories.@(js|ts)'],
+  addons: ['@storybook/addon-controls']
 }
 
 export default config
